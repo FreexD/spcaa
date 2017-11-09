d74ee10ed41f6badafa059a838b9112c72734fe6
student24 <student24@pcoip.ki.agh.edu.pl>
2017-11-09 13:45:33 +0100

    lab3

--- E2.java
+++ E2.java
@@ -7,8 +7,18 @@ class E2 extends Production {
 
 	Vertex apply(Vertex T) {
 		System.out.println("E2");
-		// implement production E2 here
-		// ...
+		T.m_b[0] /= T.m_a[0][0];
+		T.m_a[0][2] /= T.m_a[0][0];
+		T.m_a[0][1] /= T.m_a[0][0];
+		T.m_a[0][0] /= T.m_a[0][0];
+		T.m_b[1] -= T.m_b[0] * T.m_a[1][0];
+		T.m_a[1][2] -= T.m_a[0][2] * T.m_a[1][0];
+		T.m_a[1][1] -= T.m_a[0][1] * T.m_a[1][0];
+		T.m_a[1][0] -= T.m_a[0][0] * T.m_a[1][0];
+		T.m_b[2] -= T.m_b[0] * T.m_a[2][0];
+		T.m_a[2][2] -= T.m_a[0][2] * T.m_a[2][0];
+		T.m_a[2][1] -= T.m_a[0][1] * T.m_a[2][0];
+		T.m_a[2][0] -= T.m_a[0][0] * T.m_a[2][0];
 		return T;
 	}
 }
