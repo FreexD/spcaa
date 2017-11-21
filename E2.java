92ee3e67311a03b429efb0aaedb9b3cb4b264519
Michał Wolny <freexd@users.noreply.github.com>
2017-11-21 13:55:03 +0100

    prgrss

--- E2.java
+++ E2.java
@@ -1,24 +1,29 @@
 import java.util.concurrent.CyclicBarrier;
 
 class E2 extends Production {
-	E2(Vertex Vert, CyclicBarrier Barrier) {
-		super(Vert, Barrier);
-	}
+    E2(Vertex Vert, CyclicBarrier Barrier) {
+        super(Vert, Barrier);
+    }
 
-	Vertex apply(Vertex T) {
-		System.out.println("E2");
-		T.m_b[0] /= T.m_a[0][0];
-		T.m_a[0][2] /= T.m_a[0][0];
-		T.m_a[0][1] /= T.m_a[0][0];
-		T.m_a[0][0] /= T.m_a[0][0];
-		T.m_b[1] -= T.m_b[0] * T.m_a[1][0];
-		T.m_a[1][2] -= T.m_a[0][2] * T.m_a[1][0];
-		T.m_a[1][1] -= T.m_a[0][1] * T.m_a[1][0];
-		T.m_a[1][0] -= T.m_a[0][0] * T.m_a[1][0];
-		T.m_b[2] -= T.m_b[0] * T.m_a[2][0];
-		T.m_a[2][2] -= T.m_a[0][2] * T.m_a[2][0];
-		T.m_a[2][1] -= T.m_a[0][1] * T.m_a[2][0];
-		T.m_a[2][0] -= T.m_a[0][0] * T.m_a[2][0];
-		return T;
-	}
+    Vertex apply(Vertex T) {
+        System.out.println("E2");
+
+        double divider = T.m_a[0][0];
+
+        for(int i = 0; i < 3; ++i) {
+            T.m_a[0][i] /= divider;
+        }
+
+        T.m_b[0] /= divider;
+
+        for(int i = 1; i < 3; ++i) {
+            double multiplier = T.m_a[i][0];
+            T.m_a[i][0] = 0;
+            for(int j = 1; j < 3; ++j) {
+                T.m_a[i][j] -= multiplier * T.m_a[0][j];
+            }
+            T.m_b[i] -= multiplier * T.m_b[0];
+        }
+        return T;
+    }
 }
--- Executor.java
+++ Executor.java
@@ -2,50 +2,90 @@ import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.BrokenBarrierException;
 
 class Executor extends Thread {
-	public synchronized void run() {
-		Vertex S = new Vertex(null,null,null,"S");
-		try {
-			//[(P1)]
-			CyclicBarrier barrier = new CyclicBarrier(1+1);
-			P1 p1 = new P1(S,barrier);
-			p1.start();
-			barrier.await();
-			//[(P2)1(P2)2]
-			barrier = new CyclicBarrier(2+1);
-			P2 p2a = new P2(p1.m_vertex.m_left,barrier);
-			P2 p2b = new P2(p1.m_vertex.m_right,barrier);
-			p2a.start();
-			p2b.start();
-			barrier.await();
-			//[(P2)3(P2)4]
-			barrier = new CyclicBarrier(2+1);
-			P2 p2c = new P2(p2a.m_vertex.m_left,barrier);
-			P2 p2d = new P2(p2a.m_vertex.m_right,barrier);
-			p2c.start();
-			p2d.start();
-			barrier.await();
-			//[(P2)1(P2)2(P2)3(P2)4(P3)5(P3)6]
-			barrier = new CyclicBarrier(6+1);
-			P3 p3a = new P3(p2a.m_vertex.m_left,barrier);
-			P3 p3b = new P3(p2a.m_vertex.m_right,barrier);
-			P3 p3c = new P3(p2c.m_vertex.m_left,barrier);
-			P3 p3d = new P3(p2c.m_vertex.m_right,barrier);
-			P3 p3e = new P3(p2d.m_vertex.m_left,barrier);
-			P3 p3f = new P3(p2d.m_vertex.m_right,barrier);
-			p3a.start();
-			p3b.start();
-			p3c.start();
-			p3d.start();
-			p3e.start();
-			p3f.start();
-			barrier.await();
-			// MULTI-FRONTAL SOLVER ALGORITHM
-			System.out.println("done");
-			GraphDrawer drawer = new GraphDrawer();
-			drawer.draw(S);
+    public synchronized void run() {
+        Vertex S = new Vertex(null, null, null, "S");
+        try {
+            //[(P1)]
+            CyclicBarrier barrier = new CyclicBarrier(1 + 1);
+            P1 p1 = new P1(S, barrier);
+            p1.start();
+            barrier.await();
+            //[(P2)1(P2)2]
+            barrier = new CyclicBarrier(2 + 1);
+            P2 p2a = new P2(p1.m_vertex.m_left, barrier);
+            P2 p2b = new P2(p1.m_vertex.m_right, barrier);
+            p2a.start();
+            p2b.start();
+            barrier.await();
+            //[(P2)3(P2)4(P3)5(P3)6]
+            barrier = new CyclicBarrier(4 + 1);
+            P3 p3c = new P3(p2a.m_vertex.m_left, barrier);
+            P3 p3d = new P3(p2a.m_vertex.m_right, barrier);
+            P3 p3e = new P3(p2b.m_vertex.m_left, barrier);
+            P3 p3f = new P3(p2b.m_vertex.m_right, barrier);
+            p3c.start();
+            p3d.start();
+            p3e.start();
+            p3f.start();
+            barrier.await();
+            // MULTI-FRONTAL SOLVER ALGORITHM
+            //[(A1)(A)1(A)2(AN)]
+            barrier = new CyclicBarrier(4 + 1);
+            A1 localMat1 = new A1(p3c.m_vertex, barrier);
+            A localMat2 = new A(p3d.m_vertex, barrier);
+            A localMat3 = new A(p3e.m_vertex, barrier);
+            AN localMat4 = new AN(p3f.m_vertex, barrier);
+            localMat1.start();
+            localMat2.start();
+            localMat3.start();
+            localMat4.start();
+            barrier.await();
+            //[(A2)1(A2)2(A2)3]
+            barrier = new CyclicBarrier(2 + 1);
+            A2 mergedMat1 = new A2(p2a.m_vertex, barrier);
+            A2 mergedMat2 = new A2(p2b.m_vertex, barrier);
+            mergedMat1.start();
+            mergedMat2.start();
+            barrier.await();
+            //[(E2)1(E2)2(E2)3]
+            barrier = new CyclicBarrier(2 + 1);
+            E2 gaussElimMat1 = new E2(p2a.m_vertex, barrier);
+            E2 gaussElimMat2 = new E2(p2b.m_vertex, barrier);
+            gaussElimMat1.start();
+            gaussElimMat2.start();
+            barrier.await();
+            //[(Aroot)]
+            barrier = new CyclicBarrier(1 + 1);
+            Aroot mergedRootMat = new Aroot(p1.m_vertex, barrier);
+            mergedRootMat.start();
+            barrier.await();
 
-		} catch (InterruptedException | BrokenBarrierException e) {
-			e.printStackTrace();
-		}
-	}
+            barrier = new CyclicBarrier(1 + 1);
+            Eroot eroot = new Eroot(p1.m_vertex, barrier);
+            eroot.start();
+            barrier.await();
+
+            barrier = new CyclicBarrier(2);
+            BS backSubRoot = new BS(eroot.m_vertex, barrier);
+            backSubRoot.start();
+            barrier.await();
+            //[(BS)2(BS)3]
+            barrier = new CyclicBarrier(3);
+            BS backSubLeft = new BS(backSubRoot.m_vertex.m_left, barrier);
+            BS backSubRight = new BS(backSubRoot.m_vertex.m_right, barrier);
+            backSubLeft.start();
+            backSubRight.start();
+            barrier.await();
+
+            //test
+            if (p1.m_vertex.m_a[0][0] == 1.0 & p1.m_vertex.m_a[0][1] == -0.5 && p1.m_vertex.m_a[0][2] == -0.5 &
+                    p1.m_vertex.m_a[1][0] == 0.0 & p1.m_vertex.m_a[1][1] == 1.0 && p1.m_vertex.m_a[1][2] == 0.0 &
+                    p1.m_vertex.m_a[2][0] == -0.5 & p1.m_vertex.m_a[2][1] == 0.0 && p1.m_vertex.m_a[2][2] == 0.5 &
+                    p1.m_vertex.m_b[0] == 0.0 & p1.m_vertex.m_b[1] == 0.0 && p1.m_vertex.m_b[2] == 0.25)
+                System.out.println("OK");
+
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
 }
